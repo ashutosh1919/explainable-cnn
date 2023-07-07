c935ecae57b7e94df5368324f7e47bf7cdd9f2b0
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2023-07-06 23:17:54 -0700

    Creating new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="explainable_cnn",
-    version="0.0.9",
+    version="1.0.0",
     author="Ashutosh Hathidara",
     author_email="ashutoshhathidara98@gmail.com",
     description="📦 Flexible visualization package for generating" +
